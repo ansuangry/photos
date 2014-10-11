dddf87fd1e01d4d6fce2112b370cb1622a4332ba
nayan9229 <nayan9229@gmail.com>
2014-10-12 00:38:02 +0530

    parse init

--- getphotos.php
+++ getphotos.php
@@ -1,6 +1,14 @@
 <?php
 
 require '../parse-php-sdk/autoload.php';
+$path = require '../parse-php-sdk/autoload.php';
+echo "string";
+echo $path ;
 
-echo require '../parse-php-sdk/autoload.php';
+$app_id = "L65oTrxZJ2ez6CZB4S1Pwgof29tfvzocufbEO06t" ;
+$rest_key ="n12lYBuHY7tGTL7a999GPUqqG7KeIObS5kzDj6Yj";
+$master_key ="2EmnrxWrkPsIyRvIjtu3ZTgnqja38W0Ue3oJly0q";
+ParseClient::initialize( $app_id, $rest_key, $master_key );
+
+
 ?>
