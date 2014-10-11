6b7a736b5181c6b82aef98c6987572c08dbd9d6f
ansu shah <ansuangry@ymail.com>
2014-10-11 19:29:59 +0000

    parse autoload

--- getphotos.php
+++ getphotos.php
@@ -1,9 +1,11 @@
 <?php
+echo "1";
+ // Establish Connection with Parse Database
+    require_once 'parse-php-sdk/autoload.php';
+echo "2";
+    use Parse\ParseClient;
 
-require '../parse-php-sdk/autoload.php';
-$path = require '../parse-php-sdk/autoload.php';
 echo "string";
-echo $path ;
 
 $app_id = "L65oTrxZJ2ez6CZB4S1Pwgof29tfvzocufbEO06t" ;
 $rest_key ="n12lYBuHY7tGTL7a999GPUqqG7KeIObS5kzDj6Yj";
